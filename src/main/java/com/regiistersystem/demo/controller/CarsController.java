1760009e37ea480ab5168deddf7c8ab84543aca9
Daria Supińska <dariasupinska@gmail.com>
2021-08-06 02:37:19 +0200

    carList, create and getCarById endpoints added

--- src/main/java/com/regiistersystem/demo/controller/CarsController.java
+++ src/main/java/com/regiistersystem/demo/controller/CarsController.java
@@ -1,4 +1,27 @@
 package com.regiistersystem.demo.controller;
 
+import com.regiistersystem.demo.model.Car;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/cars")
 public class CarsController {
+    @GetMapping
+    public List<Car> carList(){
+        List<Car> cars = new ArrayList<>();
+        return cars;
+    }
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public void create(@RequestBody Car car){
+        carList().add(new Car());
+    }
+    @GetMapping("/{id}")
+    public Car getCarById(@PathVariable("id") long id){
+        return new Car();
+    }
 }
